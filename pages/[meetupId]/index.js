fc600c27189eb6a00d59e55a1b9c57367beda395
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-04-21 17:27:26 +0300

    added pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+	return (
+		<MeetupDetail
+			image={
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+			}
+			title={"A First Meetup"}
+			address={"15 Yemen Road, Yemen"}
+			description={"s"}
+		/>
+	);
+};
+
+export default MeetupDetails;
